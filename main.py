0808609f5a3ff391aa3d49e0ed420dc29bbdd7e3
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-31 04:35:11 -0400

    final version

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def translate(word):
         return data[word]
     elif word.title() in data: #if user entered "texas" this will check for "Texas" as well.
         return data[word.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
     elif len(get_close_matches(word ,data.keys())) > 0:
         # //return "Did ypu mean %s instead" % get_close_matches(word ,data.keys())[0]
         yn = input(f"Did you mean {get_close_matches(word ,data.keys())[0]} instead ? Please enter Y if yes or N if no : ")
