1f2f8a471ab20bdfada8e288a1096be59697f50d
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-31 06:05:46 -0400

    sucessfully connected to db and feteched data

--- main_sql.py
+++ main_sql.py
@@ -9,6 +9,16 @@ con = mysql.connector.connect(
     host = "127.0.0.1",
     port = '3307',
     database = "lab_4_n01324321"
+
 )
+
+cursor = con.cursor()
+word = input("Enter a word : ")
+query = cursor.execute("SELECT * FROM sudhir Where ID = '%s' " % word)
+results = cursor.fetchall()
+
+for i in results:
+     print(i[1])
+
 
 
