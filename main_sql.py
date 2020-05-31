2cab0fc21535141cb73ddbb431dd962487e97a7b
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-31 05:23:39 -0400

    adding sql file

---
+++ main_sql.py
@@ -0,0 +1,14 @@
+#  Dictionary Project using SQL
+
+import mysql.connector
+
+con = mysql.connector.connect(
+    
+    user = "root",
+    password = "",
+    host = "127.0.0.1",
+    port = '3307',
+    database = "lab_4_n01324321"
+)
+
+
