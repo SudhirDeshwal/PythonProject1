c670938c7ef7abf215f91dfe8ed52125b74715f2
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-31 04:11:13 -0400

    matching fuction added

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
+    word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word ,data.keys())) > 0:
+        # //return "Did ypu mean %s instead" % get_close_matches(word ,data.keys())[0]
+        yn = input(f"Did you mean {get_close_matches(word ,data.keys())[0]} instead ? Please enter Y if yes or N if no : ")
+        if yn == "Y":
+            return data[get_close_matches(word ,data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist ,please double check it."
+        else:
+            return "We didn't undertand your query. Please recheck it."
+
     else:
         return "The word doesn't exist , Please double check it."
 
