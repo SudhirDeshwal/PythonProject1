d4aa49c01dd03edb0d1beddf8449af1ae715c1fa
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-06-12 22:10:30 -0400

    comment added

--- main_sql.py
+++ main_sql.py
@@ -11,6 +11,7 @@ database = "ardit700_pm1database"
 
 cursor = con.cursor()
 
+#for input
 word=input("Enter the word: ")
 
 query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'" % word)
